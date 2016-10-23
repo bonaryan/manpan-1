4401cfedfccc73f48cd55cc59e5e9f1c4f32e6e5
Panagiotis Manoleas <manpan@ltu.se>
2016-10-23 16:10:31 +0200

    Output files initialise
    I created 2 empty files, one for a code for CUFSM output and one for
    Abaqus. On the end of the main_v1 one of the two or both the output
    scripts should be called, regarding users selection.

---
+++ Polygoner_ABQS_out.m
@@ -0,0 +1,2 @@
+% Script that prints a .jnl ASCII file for Abaqus. The .jnl creates in
+% Abaqus all the models from a given set of xy points.
---
+++ Polygoner_CFSM_out.m
@@ -0,0 +1,3 @@
+% Script that creates and saves a matlab database in the right format for
+% CUFSM xrom a given set of x, y arrays
+
