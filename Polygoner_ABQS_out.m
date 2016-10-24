fe35809069ccfe681f41aea820d8c579b585568a
bonaryan <bona_ryan04@yahoo.com>
2016-10-24 23:51:06 +0200

    test: attempt to make .jnl text file
    still far from finished

--- Polygoner_ABQS_out.m
+++ Polygoner_ABQS_out.m
@@ -2,42 +2,12 @@
 % Abaqus all the models from a given set of xy points.
 
 
-% attempt to write to multiple lines to file
-jnltext = '# Save by user on 2016_10_24-12.23.43; build 6.14-1 2014_06_05-00.11.02 134264\r\n% from part import *'
-% from material import *
-% from section import *
-% from assembly import *
-% from step import *
-% from interaction import *
-% from load import *
-% from mesh import *
-% from optimization import *
-% from job import *
-% from sketch import *
-% from visualization import *
-% from connectorBehavior import *
-% mdb.models.changeKey(fromName='Model-1', toName='p111')
-% mdb.models['p111'].ConstrainedSketch(name='__profile__', sheetSize=1200.0)
-% mdb.models['p111'].sketches['__profile__'].Line(point1=(-245.0, 45.0), point2=(
-%     -195.0, 0.0))
-% mdb.models['p111'].sketches['__profile__'].Line(point1=(-195.0, 0.0), point2=(
-%     0.0, 110.0))
-% mdb.models['p111'].sketches['__profile__'].Line(point1=(0.0, 110.0), point2=(
-%     205.0, 0.0))
-% mdb.models['p111'].sketches['__profile__'].Line(point1=(205.0, 0.0), point2=(
-%     260.0, 40.0))
-% mdb.models['p111'].Part(dimensionality=THREE_D, name='sector', type=
-%     DEFORMABLE_BODY)
-% mdb.models['p111'].parts['sector'].BaseShellExtrude(depth=1200.0, sketch=
-%     mdb.models['p111'].sketches['__profile__'])
-% del mdb.models['p111'].sketches['__profile__']
-% mdb.models['p111'].Material(name='pure-elastic')
-% mdb.models['p111'].materials['pure-elastic'].Elastic(table=((210000.0, 0.3), ))
-% # Save by user on 2016_10_24-12.25.31; build 6.14-1 2014_06_05-00.11.02 134264
-% '
+% attempt to write to multiple lines to file (not finished yet, need to include variable in the text and combine all models) 
+jnltext = ['from part import *' char(10) 'from material import *' char(10) 'from section import *' char(10) 'from assembly import *' char(10) 'from step import *' char(10) 'from interaction import *' char(10) 'from load import *' char(10) 'from mesh import *' char(10) 'from optimization import *' char(10) 'from job import *' char(10) 'from sketch import *' char(10) 'from visualization import *' char(10) 'from connectorBehavior import *' char(10) 'mdb.models.changeKey(fromName=''Model-1'',toName=''p111'')' char(10) 'mdb.models[''p111''].ConstrainedSketch(name=''__profile__'', sheetSize=1200.0)' char(10) 'mdb.models[''p111''].sketches[''__profile__''].Line(point1=(-245.0, 45.0), point2=(-195.0, 0.0))' char(10) 'mdb.models[''p111''].sketches[''__profile__''].Line(point1=(-195.0, 0.0), point2=(0.0, 110.0))' char(10) 'mdb.models[''p111''].sketches[''__profile__''].Line(point1=(0.0, 110.0), point2=(205.0, 0.0))' char(10) 'mdb.models[''p111''].sketches[''__profile__''].Line(point1=(205.0, 0.0), point2=(260.0, 40.0))' char(10) 'mdb.models[''p111''].Part(dimensionality=THREE_D, name=''sector'', type=DEFORMABLE_BODY)' char(10) 'mdb.models[''p111''].parts[''sector''].BaseShellExtrude(depth=1200.0, sketch=mdb.models[''p111''].sketches[''__profile__''])' char(10) 'del mdb.models[''p111''].sketches[''__profile__'']' char(10) 'mdb.models[''p111''].Material(name=''pure-elastic'')' char(10) 'mdb.models[''p111''].materials[''pure-elastic''].Elastic(table=((210000.0, 0.3), ))']
 
 fid = fopen('polygons.jnl', 'w');
 
 fwrite(fid, jnltext);
 
 fclose(fid);
+
