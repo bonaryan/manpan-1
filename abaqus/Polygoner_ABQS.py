4ffadc43aed87d2ecc5b6eb0307ab30975215926
Panagiotis Manoleas <manpan@ltu.se>
2016-12-05 02:14:20 +0100

    ABQS Tie constraints
    first attempt for bolt ties. it runs for a few bolts
    work in progress

--- abaqus/Polygoner_ABQS.py
+++ abaqus/Polygoner_ABQS.py
@@ -1,4 +1,3 @@
-import pickle
 import numpy as np
 from part import *
 from material import *
@@ -13,8 +12,13 @@ from job import *
 from sketch import *
 from visualization import *
 from connectorBehavior import *
+session.journalOptions.setValues(replayGeometry=COORDINATE, recoverGeometry=COORDINATE)
+# Import profiles database and profile metadata --------------------------------------------------------------
 
-# Import profiles database and profile metadata
+# Import pickle to load the .pkl database
+import pickle
+
+# Open and read the database
 profiles_file = open("profiles.pkl",'rb')
 profiles = pickle.load(profiles_file)
 profiles_file.close()
@@ -23,7 +27,6 @@ profiles_file = open("meta.pkl",'rb')
 profiles_meta = pickle.load(profiles_file)
 profiles_file.close()
 
-## 1st Phase: Buckling analysis
 
 #for i in range(profiles.shape[0]):
 #	for j in range(profiles.shape[1]):
@@ -33,6 +36,7 @@ for i in range(1):
 	for j in range(1):
 		for k in range(1):
 			for l in range(1):
+				
 				# Variables holding information of the current profile
 				current_model = str(i+1)+'-'+str(j+1)+'-'+str(k+1)+'-'+str(l+1)
 				current_d = float(profiles_meta[i][j][k][l][0][0])
@@ -96,9 +100,14 @@ for i in range(1):
 					
 					# Washer diameter
 					d_washer = 30
+					
+					# Initiate list to store datum planes
+					datum_p=[]
 										
 					# Make holes
+										
 					for o in range(int(bolts_z.shape[0])):
+												
 						sector_part.HoleBlindFromEdges(
 							depth=1.0,
 							diameter=d_washer,
@@ -122,14 +131,17 @@ for i in range(1):
 							)
 						
 						# Create datum planes to be used for partitioning the sector
-						sector_part.DatumPlaneByPrincipalPlane(
+						
+						datum1=sector_part.DatumPlaneByPrincipalPlane(
 							offset=bolts_z[o]-bolts_w, 
 							principalPlane=XYPLANE
 							)
-						sector_part.DatumPlaneByPrincipalPlane(
+						datum2=sector_part.DatumPlaneByPrincipalPlane(
 							offset=bolts_z[o]+bolts_w, 
 							principalPlane=XYPLANE
 							)
+						datum_p.append(datum1)
+						datum_p.append(datum2)
 					
 					# Partition the sector
 					
@@ -137,8 +149,9 @@ for i in range(1):
 					n_dat = int(len(sector_part.datums))
 					
 					# cut all the faces using the datum planes
-#					for o in range((n_dat-2)/3):
-					for o in range(1):						
+										
+#					for o in range((n_dat-2)):
+					for o in range(2):
 						sector_part.PartitionFaceByDatumPlane(
 							datumPlane=sector_part.datums.items()[o+1][1],
 							faces=sector_part.faces[:]
@@ -257,10 +270,12 @@ for i in range(1):
 						)
 					
 					# Material --------------------------------------------------------------------------------------------------------------------
+					
 					c_model.Material(name='pure-elastic')
 					c_model.materials['pure-elastic'].Elastic(table=((210000.0, 0.3), ))
 		
 					# Create sections -------------------------------------------------------------------------------------------------------------
+					
 					# -for sector
 					c_model.HomogeneousShellSection(
 						idealization=NO_IDEALIZATION, 
@@ -324,36 +339,36 @@ for i in range(1):
 						thicknessAssignment=FROM_SECTION
 						)
 					
-					# Meshing ---------------------------------------------------------------------------------------------------------------------
-					
-					# Global seeding in mm
-					seedsize = 30
-					
-					# -Sector
-					sector_part.setMeshControls(
-						algorithm=MEDIAL_AXIS,
-						elemShape=QUAD, 
-						regions=sector_part.faces[:]
-						)
-					sector_part.seedPart(
-						deviationFactor=0.1, 
-						minSizeFactor=0.1,
-						size=seedsize
-						)
-					sector_part.generateMesh()
-					
-					# -Gusset
-					gusset_part.setMeshControls(
-						algorithm=MEDIAL_AXIS,
-						elemShape=QUAD,
-						regions=gusset_part.faces[:]
-						)
-					gusset_part.seedPart(
-						deviationFactor=0.1, 
-						minSizeFactor=0.1,
-						size=seedsize
-						)
-					gusset_part.generateMesh()
+#					# Meshing ---------------------------------------------------------------------------------------------------------------------
+#					
+#					# Global seeding in mm
+#					seedsize = 30
+#					
+#					# -Sector
+#					sector_part.setMeshControls(
+#						algorithm=MEDIAL_AXIS,
+#						elemShape=QUAD, 
+#						regions=sector_part.faces[:]
+#						)
+#					sector_part.seedPart(
+#						deviationFactor=0.1, 
+#						minSizeFactor=0.1,
+#						size=seedsize
+#						)
+#					sector_part.generateMesh()
+#					
+#					# -Gusset
+#					gusset_part.setMeshControls(
+#						algorithm=MEDIAL_AXIS,
+#						elemShape=QUAD,
+#						regions=gusset_part.faces[:]
+#						)
+#					gusset_part.seedPart(
+#						deviationFactor=0.1, 
+#						minSizeFactor=0.1,
+#						size=seedsize
+#						)
+#					gusset_part.generateMesh()
 					
 					# Create assembly -------------------------------------------------------------------------------------------------------------
 					
@@ -361,7 +376,7 @@ for i in range(1):
 					c_assembly.DatumCsysByDefault(CARTESIAN)
 					
 					# -Sectors
-					c_assembly.Instance(
+					s1_instance=c_assembly.Instance(
 						dependent=ON,
 						name='sector-1',
 						part=sector_part
@@ -369,12 +384,15 @@ for i in range(1):
 					c_assembly.DatumAxisByPrincipalAxis(
 						principalAxis=ZAXIS
 						)
-					c_assembly.RadialInstancePattern(
+					s3_instance=c_assembly.RadialInstancePattern(
 						axis=(0.0, 0.0, 1.0), 
 						instanceList=('sector-1', ),
 						number=3, point=(0.0, 0.0, 0.0),
 						totalAngle=240.0
 						)
+					
+					s2_instance=s3_instance[0]
+					s3_instance=s3_instance[1]
 		
 					# -Gusset plate
 					
@@ -396,19 +414,221 @@ for i in range(1):
 						)
 					
 					# --Translate them to the right position
-					g2_instance.translate(vector=(
-						0.0, 0.0, (current_l + current_d)))
-					g3_instance.translate(vector=(
-						0.0, 0.0, 2*(current_l + current_d)))
+					g2_instance.translate(
+						vector=(0.0, 0.0, (current_l + current_d))
+						)
+					g3_instance.translate(
+						vector=(0.0, 0.0, 2*(current_l + current_d))
+						)
+					
+					# Interactions ---------------------------------------------------------------------
+					
+					# Create sets node regions to be used for the tie and coupling constraints
+					
+					holes11=()
+					holes12=()
+					holes21=()
+					holes22=()
+					holes31=()
+					holes32=()
+					gholes1=()
+					gholes2=()
+					gholes3=()
+					
+					# Position of the holes on the cross-section
+					sh11 = np.array([profiles[i][j][k][0][1], profiles[i][j][k][1][1]])
+					sh12 = np.array([profiles[i][j][k][0][-2], profiles[i][j][k][1][-2]])
+					
+					gh1 = (gp1[0]-bolts_w*cos(5*pi/6), gp1[1]-bolts_w*sin(5*pi/6))
+					gh2 = (gp2[0]-bolts_w*cos(-pi/2), gp2[1]-bolts_w*sin(-pi/2))
+					gh3 = (gp3[0]-bolts_w*cos(pi/6), gp3[1]-bolts_w*sin(pi/6))  
+						  
+						  
+					# Rotation matrix to multiply the previous point in order to get the points of the other 2 gusset fins
+					Rmat = np.array([[cos(-2*pi/3), -sin(-2*pi/3)], [sin(-2*pi/3), cos(-2*pi/3)]])
+					
+					# Calculate the end points of the other 2 gusset fins by multiplying with the 120 degrees rotation matrix
+					sh21 = sh11.dot(Rmat)
+					sh22 = sh12.dot(Rmat)
+					
+					sh31 = sh21.dot(Rmat)
+					sh32 = sh22.dot(Rmat)
+					
+					bolts_zg = (tuple(bolts_z1)+ tuple(bolts_z3)+ tuple(bolts_z5))
+					for o in (bolts_zg):
+						# Construct a list with the coordinates of all the bolt hole centres
+						
+						holes11=holes11+((tuple(sh11)[0], tuple(sh11)[1], o),)
+						holes12=holes12+((tuple(sh12)[0], tuple(sh12)[1], o),)
+						
+						holes21=holes21+((tuple(sh21)[0], tuple(sh21)[1], o),)
+						holes22=holes22+((tuple(sh22)[0], tuple(sh22)[1], o),)
+						
+						holes31=holes31+((tuple(sh31)[0], tuple(sh31)[1], o),)
+						holes32=holes32+((tuple(sh32)[0], tuple(sh32)[1], o),)
+						
+						gholes1 = gholes1+((gh1[0], gh1[1], o),)
+						gholes2 = gholes2+((gh2[0], gh2[1], o),)
+						gholes3 = gholes3+((gh2[0], gh3[1], o),)
 					
 					# Create reference points for BCs/loads.
 					
 					# -RPs for the faces at the two ends of the columns
-					c_assembly.ReferencePoint(point=(0.0, 0.0, 0.0))
-					c_assembly.ReferencePoint(point=(0.0, 0.0, (2*current_l + 3*current_d)))
+					c_assembly.ReferencePoint((0.0, 0.0, 0.0))
+					c_assembly.ReferencePoint((0.0, 0.0, (2*current_l + 3*current_d)))
 					
 					# - RP at the middle
-					c_assembly.ReferencePoint(point=(0.0, 0.0, (current_l + 1.5*current_d)))
+					c_assembly.ReferencePoint((0.0, 0.0, (current_l + 1.5*current_d)))
+					
+					
+					# Bolt tie constraints
+					
+					for o in range(len(bolts_zg)):
+						set1 = c_assembly.Set(
+							edges=g1_instance.edges.getByBoundingSphere(
+								center=gholes1[o],
+								radius=d_washer/2+1
+								),
+							name='b'+str(o)+'-gusset'
+							)
+							
+						set2 = c_assembly.Set(
+							edges=s1_instance.edges.findAt(((holes11[o][0], holes11[o][1], holes11[o][2]-d_washer/2), ), )+\
+							s2_instance.edges.findAt(((holes22[o][0], holes22[o][1], holes22[o][2]-d_washer/2), ), ),
+							name='b'+str(o)+'-sector'
+							)
+							
+						c_model.Tie(
+							adjust=ON,
+							master=c_assembly.sets['b'+str(o)+'-gusset'],
+							name='b'+str(o)+'-tie', 
+							positionToleranceMethod=COMPUTED,
+							slave=c_assembly.sets['b'+str(o)+'-sector'],
+							thickness=ON, 
+							tieRotations=ON
+							)
+					
+					
+#					# - End face couplings to reference points
+#					
+#					c_assembly.Set(
+#						name='RP-1-set', 
+#						referencePoints=(c_assembly.referencePoints.findAt((0, 0, 0)), )
+#						)
+#					
+#					c_assembly.Set(
+#						g1_instance.edges.findAt(
+#							((32.563453, 18.800518, 0.0), ),
+#							((0.0, -160.904145, 0.0), ),
+#							((-32.563453, 18.800518, 0.0), ),
+#							((139.347077, 80.452073, 0.0), ),
+#							((0.0, -37.601036, 0.0), ),
+#							((-139.347077, 80.452073, 0.0), ), 
+#							),
+#						name='end1-face'
+#						)
+#					
+#					c_model.Coupling(
+#						controlPoint=c_assembly.sets['RP-1-set'], 
+#						couplingType=KINEMATIC,
+#						influenceRadius=WHOLE_SURFACE,
+#						localCsys=None,
+#						name='end1-coupling', 
+#						surface=c_assembly.sets['end1-face'],
+#						u1=ON, u2=ON, u3=ON, ur1=ON, ur2=ON, ur3=ON
+#						)
+#						
+#					c_assembly.Set(
+#						name='RP-2-set',
+#						referencePoints=(c_assembly.referencePoints[18], )
+#						)
+#					
+#					c_assembly.Set(
+#						g3_instance.edges.findAt(
+#							((97.690358, 56.401554, 11090.532103), ),
+#							((0.0, -160.904145, 11090.532103), ),
+#							((-97.690358, 56.401554, 11090.532103), ),
+#							((157.533611, 90.952073, 11090.532103), ),
+#							((0.0, -37.601036, 11090.532103), ),
+#							((-157.533611, 90.952073, 11090.532103), ), 
+#							),
+#						name='end2-face'
+#						)
+#						
+#					c_model.Coupling(
+#						controlPoint=c_assembly.sets['RP-2-set'], 
+#						couplingType=KINEMATIC, influenceRadius=WHOLE_SURFACE,
+#						localCsys=None,
+#						name='end2-coupling', 
+#						surface=c_assembly.sets['end2-face'],
+#						u1=ON, u2=ON, u3=ON, ur1=ON, ur2=ON, ur3=ON
+#						)
+#					
+#
+#					
+#					# Boundary Conditions --------------------------------------------------------------
+#
+#					
+#					# BCs					
+#					end1-BC=mdb.models['1-1-1-1'].DisplacementBC(
+#						amplitude=UNSET, 
+#						createStepName='Initial', 
+#						distributionType=UNIFORM, 
+#						fieldName='', 
+#						localCsys=None, 
+#						name='fix-end1', 
+#						region=Region(referencePoints=(mdb.models['1-1-1-1'].rootAssembly.referencePoints[15], )), 
+#						u1=SET, u2=SET, u3=SET, ur1=UNSET, ur2=UNSET, ur3=UNSET
+#						)
+#						
+#					end1-BC=mdb.models['1-1-1-1'].DisplacementBC(
+#						amplitude=UNSET,
+#						createStepName='Initial', 
+#						distributionType=UNIFORM, 
+#						fieldName='', 
+#						localCsys=None, 
+#						name='fix-end2', 
+#						region=Region(referencePoints=(mdb.models['1-1-1-1'].rootAssembly.referencePoints[16], )), 
+#						u1=SET, u2=SET, u3=SET, ur1=UNSET, ur2=UNSET, ur3=UNSET
+#						)
+#					
+#					# Step -----------------------------------------------------------------------------------
+#					
+#					load_step = c_model.StaticStep(name='Load', previous='Initial')
+#					
+#					
+#					c_assembly.Set(name='m_Set-1', referencePoints=(
+#						c_assembly.referencePoints[17], ))
+#					c_assembly.Set(edges=
+#						c_assembly.instances['sector-1-rad-2'].edges.getSequenceFromMask(
+#						mask=('[#49249244 #92492492 #924 ]', ), )+\
+#						c_assembly.instances['sector-1-rad-2-rad-2'].edges.getSequenceFromMask(
+#						mask=('[#49249244 #92492492 #924 ]', ), )+\
+#						c_assembly.instances['sector-1'].edges.getSequenceFromMask(
+#						mask=('[#49249244 #92492492 #924 ]', ), ), name='s_Set-1')
+#					c_model.Coupling(controlPoint=
+#						c_assembly.sets['m_Set-1'], couplingType=KINEMATIC, 
+#						influenceRadius=WHOLE_SURFACE, localCsys=None, name='Constraint-1', 
+#						surface=c_assembly.sets['s_Set-1'], u1=ON, u2=ON, u3=
+#						ON, ur1=ON, ur2=ON, ur3=ON)
+#						
+#					# -Face 2
+#					c_assembly.Set(name='m_Set-3', referencePoints=(
+#						c_assembly.referencePoints[18], ))
+#					c_assembly.Set(edges=
+#						c_assembly.instances['sector-1-rad-2'].edges.getSequenceFromMask(
+#						mask=('[#92492491 #24924924 #249 ]', ), )+\
+#						c_assembly.instances['sector-1'].edges.getSequenceFromMask(
+#						mask=('[#92492491 #24924924 #249 ]', ), )+\
+#						c_assembly.instances['sector-1-rad-2-rad-2'].edges.getSequenceFromMask(
+#						mask=('[#92492491 #24924924 #249 ]', ), ), name='s_Set-3')
+#					c_model.Coupling(controlPoint=
+#						c_assembly.sets['m_Set-3'], couplingType=KINEMATIC, 
+#						influenceRadius=WHOLE_SURFACE, localCsys=None, name='Constraint-2', 
+#						surface=c_assembly.sets['s_Set-3'], u1=ON, u2=ON, u3=
+#						ON, ur1=ON, ur2=ON, ur3=ON)
+					
+					
 					
 
 #					# Create buckling step
